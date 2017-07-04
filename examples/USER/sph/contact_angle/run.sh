7d0434e37bb17495ed4aea53cb9edd68841017f7
Quang Ha <qth20@bu.edu>
2017-07-04 19:11:02 -0400

    Change executable file

--- examples/USER/sph/contact_angle/run.sh
+++ examples/USER/sph/contact_angle/run.sh
@@ -6,5 +6,5 @@ dname=data-wall-nx${nx}
 # number of a case
 icase=2
 mkdir -p ${dname}
-lmp=../../../../src/lmp_linux
+lmp=../../../../src/lmp_mpi
 ${lmp} -in droplet.lmp -var icase ${icase} -var dname ${dname} -var nx ${nx}
