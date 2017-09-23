c05b1c7fdd3a9f4c129f6b691c58a8ed633c9bf1
Quang Ha <qth20@bu.edu>
2017-09-22 23:31:17 -0400

    Let's hope it will compile..

--- src/USER-SPH/fix_phase_change_threshold.cpp
+++ src/USER-SPH/fix_phase_change_threshold.cpp
@@ -90,8 +90,113 @@ FixPhaseChangeThreshold::FixPhaseChangeThreshold(LAMMPS *lmp, int narg, char **a
 	zlo < domain->boxlo_bound[2] || zhi > domain->boxhi_bound[2])
       error->all(FLERR,"Phase change region extends outside simulation box");
   }
+
+  // set up reneighbouring
+  force_reneighbor = 1;
+  next_reneighbor = update->ntimestep+1;
+  nfirst = next_reneighbor;
+}
+/* ---------------------------------------------------------------------- */
+FixPhaseChangeThreshold::~FixPhaseChangeThreshold()
+{
+  delete [] idregion;
+}
+/* ---------------------------------------------------------------------- */
+int FixPhaseChangeThreshold::setmask()
+{
+  int mask = 0;
+  mask |= PRE_EXCHANGE;
+  return mask;
+}
+/* ---------------------------------------------------------------------- */
+void FixPhaseChangeThreshold::init()
+{
+  // set index and check validity of region
+  iregion = domain->find_region(id_region);
+  if (iregion == -1)
+    error->all(FLERR,"Region ID for fix phase_change_threshold does not exist.");
+
+  // Need a full neighbour list, built whenever re-neighbouring occurs
+  int irequest = neighbor->request((void *) this);
+  neighbor->requests[irequest]->pair = 0;
+  neighbor->requests[irequest]->fix = 1;
+  neighbor->requests[irequest]->half = 0;
+  neighbor->requests[irequest]->full = 1;
+}
+/* ---------------------------------------------------------------------- */
+void FixPhaseChangeThreshold::init_list(int, NeighList *ptr)
+{
+  list = ptr;
+}
+/* ---------------------------------------------------------------------- */
+/* perform phase change */
+/* ---------------------------------------------------------------------- */
+void FixPhaseChangeThreshold::pre_exchange()
+{
+  // just return if should not be called on this time step
+  if (nexT_reneighbor != update->ntimestep) return;
+ 
+  // TODO: Check if atom is in sub box below or above it
+
+  int nins = 0;
+  int nlocal = atom->nlocal;
+  int* numneigh = list->numneigh;
+  double **x = atom->x;
+  double **v = atom->v;
+  double **vest = atom->vest;
+  double *rmas = atom->rmas;
+  double *rho = atom->rho;
+  double *cv = atom->cv;
+  double *e = atom->e;
+  dmass = atom->drho;
+  int *type = atom->type;
+
+  int nall;
+  if (force->newtom) nall = atom->nlocal + atom->nghost;
+  else nall = atom->nlocal;
+
+  for (int i=0; i<nall; i++)
+    {
+      dmass[i] = 0.0;
+    }
+
+  for (int i=0; i<nlocal; i++)
+    {
+      double Ti = sph_energy2t(e[i], cv[i]);
+      if ( (Ti < Tc) && type[i] != to_type)
+	{
+	  atom->type[i] = to_type;
+	}
+    }
   
 }
 /* ---------------------------------------------------------------------- */
-
+/* pack entire state of Fix into one write */
 /* ---------------------------------------------------------------------- */
+void FixPhaseChangeThreshold::write_restart(FILE *fp)
+{
+  int n = 0;
+  double list[4];
+  list[n++] = next_reneighbor;
+
+  if (comm->me == 0)
+    {
+      int size = n*sizeof(double);
+      fwrite(&size, sizeof(int), 1, fp);
+      fwrite(list, sizeof(double), n, fp);
+    }
+}
+/* ---------------------------------------------------------------------- */
+/* use state info from restart file to restart the fix /*
+/* ---------------------------------------------------------------------- */
+void FixPhaseChangeThreshold::restart(char *buf)
+{
+  int n = 0;
+  double *list = (double *) buf;
+
+  seed = static_case<int> (list[n++]);
+  nfirst = static_cast<int> (list[n++]);
+  next_reneighbor = static_cast<int> (list[n++]);
+
+  random->reset(seed);
+}
